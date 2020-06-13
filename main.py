b872acee0ec28146996a60394360c512cee17465
danwei002 <35182369+danwei002@users.noreply.github.com>
2020-06-12 23:25:16 -0400

    Started adding games

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ import random
 from random import randrange
 import asyncio
 import discord
+from discord.ext import commands
 import discord.ext.commands
 from discord.ext.commands import Bot, has_permissions, CheckFailure
+from discord.ext import tasks
 import json
 import sys
 import io
@@ -13,40 +15,50 @@ from timeit import default_timer as timer
 from PIL import Image, ImageDraw, ImageColor
 
 BOT_PREFIX = "%"
-TOKEN = ""
+TOKEN = "NzE2MzU3MTI3NzM5ODAxNzEx.XuPISw.PGp1lEC7o-R3Ixy-ONDsea4molk"
 
 client = Bot(command_prefix=BOT_PREFIX)
-
-
-@client.event
-async def on_ready():
-    loadHands()
-    print("Now online.")
-
 
 offset = 10
 cardWidth = 69
 cardHeight = 105
 hands = {}
 userSortType = {}
+players = []
+
+gameType = {"5card": 0}
+currentGame = ""
+gameStarted = False
+gameUnderway = False
+
+
+@client.event
+async def on_ready():
+    dumpData()
+    loadData()
+    print("Now online.")
 
 
 # Load user hands
-def loadHands():
-    global hands, userSortType
+def loadData():
+    global hands, userSortType, players
     with open('hands.json') as hFile:
         hands = json.load(hFile)
     with open('sortType.json') as sTFile:
         userSortType = json.load(sTFile)
+    with open('players.json') as pFile:
+        players = json.load(pFile)
 
 
 # Dump user hands
-def dumpHands():
+def dumpData():
     global hands, userSortType
     with open('hands.json', 'w') as hFile:
         json.dump(hands, hFile)
     with open('sortType.json', 'w') as sTFile:
         json.dump(userSortType, sTFile)
+    with open('players.json', 'w') as pFile:
+        json.dump(players, pFile)
 
 
 # Add card to user's hand
@@ -56,6 +68,15 @@ def addCard(user: discord.Member, card: str):
         hands[str(user.id)].append(card)
     else:
         hands.update({str(user.id): [card]})
+
+
+# Reset all game data and variables
+def resetData():
+    global gameUnderway, gameStarted, players, hands
+    gameStarted = False
+    gameUnderway = False
+    hands.clear()
+    players.clear()
 
 
 # Card generator
@@ -134,11 +155,11 @@ def sortHand(user: discord.Member):
     global hands
     global ORDER, presOrder, suitOrder, order
     h = []
-    loadHands()
+    loadData()
     if str(user.id) not in userSortType:
         userSortType.update({str(user.id): 'd'})
 
-    dumpHands()
+    dumpData()
 
     if userSortType[str(user.id)] == 'p':
         ORDER = presOrder
@@ -169,7 +190,7 @@ async def randcard(ctx):
     card = drawCard(ctx.author)
     card.save('drawn.png', format='PNG')
     file = discord.File(open('drawn.png', 'rb'))
-    dumpHands()
+    dumpData()
     await ctx.send(ctx.author.mention, file=file)
 
 
@@ -178,7 +199,7 @@ async def randcard(ctx):
     brief="Draw a number of cards from the deck",
     pass_context=True)
 async def draw(ctx, cards: int = 1):
-    loadHands()
+    loadData()
     global tempDeck
     if cards > len(tempDeck):
         await ctx.send("Not enough cards left in the deck.")
@@ -188,7 +209,7 @@ async def draw(ctx, cards: int = 1):
         tempDeck.remove(cardName)
         addCard(ctx.author, cardName)
     await ctx.send("You have received your cards " + ctx.author.mention)
-    dumpHands()
+    dumpData()
 
 
 @client.command(description="View how many cards are left in the deck.",
@@ -224,7 +245,7 @@ async def refill(ctx):
                 brief="View your hand",
                 pass_context=True)
 async def hand(ctx):
-    loadHands()
+    loadData()
     if str(ctx.author.id) not in hands:
         await ctx.send("You have no cards in your hand " + ctx.author.mention)
         return
@@ -246,10 +267,9 @@ async def hand(ctx):
     await ctx.send(ctx.author.mention, file=file)
 
 
-@client.command(
-    description="Set sorting type. Use 'p' for president-style sorting (3 low, 2 high), 'd' for default sorting (A low, K high), 's' for suit sorting (diamonds - spades).",
-    brief="Set sorting type",
-    pass_context=True)
+@client.command(description="Set sorting type. Use 'p' for president-style sorting (3 low, 2 high), 'd' for default sorting (A low, K high), 's' for suit sorting (diamonds - spades).",
+                brief="Set sorting type",
+                pass_context=True)
 async def setSort(ctx, sortType: str = None):
     global order, presOrder, ORDER
     if sortType is None:
@@ -270,16 +290,86 @@ async def setSort(ctx, sortType: str = None):
         userSortType[str(ctx.author.id)] = sortType
     else:
         userSortType.update({str(ctx.author.id): sortType})
-    dumpHands()
+    dumpData()
+
+
+@client.command(description="Start a game of 5-card draw.",
+                brief="Start a game of 5-card draw",
+                aliases=['5card'],
+                pass_context=True)
+async def fivecard(ctx):
+    global currentGame, gameStarted
+    if gameStarted:
+        await ctx.send("There is another game underway.")
+        return
+    currentGame = "5card"
+    gameStarted = True
+    await ctx.send("Five card draw game started.")
+
+
+@client.command(description="Join a game that is not yet underway.",
+                brief="Join a game that is not yet underway",
+                pass_context=True)
+async def join(ctx):
+    loadData()
+    if not gameStarted:
+        await ctx.send("There is no active game to join.")
+    elif not gameUnderway and gameStarted:
+        if players.count(str(ctx.author.id)) > 0:
+            await ctx.send("You are already in this game.")
+        else:
+            await ctx.send(ctx.author.mention + " you joined the game.")
+            players.append(str(ctx.author.id))
+    else:
+        await ctx.send("A game is already underway.")
+
+
+@client.command(description="Start the game.",
+                brief="Start the game",
+                pass_context=True)
+async def start(ctx):
+    loadData()
+    global players, gameStarted, gameUnderway
+    if not gameStarted:
+        await ctx.send("There is no game to start.")
+        return
+
+    if gameUnderway:
+        await ctx.send("The game is already underway.")
+        return
+
+    if players.count(str(ctx.author.id)) <= 0:
+        await ctx.send("You are not in this game.")
+        return
+
+    output = "Game started.\nIn this game:\n"
+    for ID in players:
+        user = client.get_user(int(ID))
+        output += user.mention + "\n"
+
+    await ctx.send(output)
+    global tempDeck, deck
+    tempDeck = ["deck/AD.png", "deck/AC.png", "deck/AH.png", "deck/AS.png", "deck/2D.png", "deck/2C.png", "deck/2H.png",
+                "deck/2S.png", "deck/3D.png", "deck/3C.png", "deck/3H.png", "deck/3S.png",
+                "deck/4D.png", "deck/4C.png", "deck/4H.png", "deck/4S.png", "deck/5D.png", "deck/5C.png", "deck/5H.png",
+                "deck/5S.png", "deck/6D.png", "deck/6C.png", "deck/6H.png", "deck/6S.png",
+                "deck/7D.png", "deck/7C.png", "deck/7H.png", "deck/7S.png", "deck/8D.png", "deck/8C.png", "deck/8H.png",
+                "deck/8S.png", "deck/9D.png", "deck/9C.png", "deck/9H.png", "deck/9S.png",
+                "deck/10D.png", "deck/10C.png", "deck/10H.png",
+                "deck/10S.png", "deck/JD.png", "deck/JC.png", "deck/JH.png", "deck/JS.png", "deck/QD.png",
+                "deck/QC.png",
+                "deck/QH.png", "deck/QS.png",
+                "deck/KD.png", "deck/KC.png", "deck/KH.png", "deck/KS.png"]
+    gameUnderway = True
 
 
 @client.command(description="Reset hands.",
                 brief="Reset hands",
                 pass_context=True)
 async def reset(ctx):
-    loadHands()
+    loadData()
     hands.clear()
-    dumpHands()
+    dumpData()
     await ctx.send("All user hands have been reset.")
 
 
@@ -289,4 +379,18 @@ async def reset_error(ctx, error):
         await ctx.send("You are not authorized to use this command.")
 
 
+class Game(commands.Cog):
+    global gameUnderway, gameStarted, currentGame
+
+    def __init__(self):
+        self.gameLoop.start()
+
+    @tasks.loop(seconds=1)
+    async def gameLoop(self):
+        dumpData()
+        if not gameStarted or not gameUnderway:
+            return
+
+
+client.add_cog(Game())
 client.run(TOKEN)
